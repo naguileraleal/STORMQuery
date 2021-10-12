a54b520e47dad3f2161017af37834d99dcaaa913
nicolas aguilera <nicolas.aguilera@fing.edu.uy>
2021-10-12 00:10:04 -0300

    issue 11: delete deprecated files

--- funciones/subfunciones/isCommand.py
+++
@@ -1,9 +0,0 @@
-# Funcion que evalua si el string recibido es un codigo de orden
-# "codigo" es un string que corresponde a un codigo de barra
-
-def is_command(barcode):
-    
-    if const.COMMANDS.get(int(barcode),'NULL') != 'NULL':
-        return barcode
-    else:
-        return False
